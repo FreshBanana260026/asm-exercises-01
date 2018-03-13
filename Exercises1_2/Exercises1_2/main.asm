bae235a177d654ec0ad6911926b509c9a439c3e4
Patrik <260026@via.dk>
2018-03-13 10:10:56 +0100

    initial commit

---
+++ Exercises1_2/Exercises1_2/main.asm
@@ -0,0 +1,10 @@
+;
+; Exercises1_2.asm
+;
+; Created: 3/12/2018 20:56:23
+; Author : Patrik
+;
+
+
+ldi r16, 0b0100_0010    ; load value into register r16
+nop                     ; does nothing, but provides a place to put a breakpoint
---
+++ Exercises1_3/Exercises1_3/main.asm
@@ -0,0 +1,12 @@
+;
+; Exercises1_3.asm
+;
+; Created: 3/12/2018 21:08:15
+; Author : Patrik
+;
+
+
+ldi r16, 42				; load value into register r16 0b0100_0010
+ldi r17, 0x33			; load hexadecimal value into register r17
+ldi r18, 0b10110010		; load binary value into register r18
+nop                     ; does nothing, but provides a place to put a breakpoint
