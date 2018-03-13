034058e4ddb54d8090c802602bc43180c22956ee
Patrik <260026@via.dk>
2018-03-13 10:44:45 +0100

    exercise 4

---
+++ Exercises1_4/Exercises1_4/main.asm
@@ -0,0 +1,12 @@
+;
+; Exercises1_4.asm
+;
+; Created: 3/13/2018 10:23:52
+; Author : Patrik
+;
+
+
+ldi r16, 0x08			; adds number 0x08 to register r16
+ldi r18, 0xb3			; adds number 0xb3 to register r17
+add r18, r16			; adds 2 numbers and result is stored in r18
+
