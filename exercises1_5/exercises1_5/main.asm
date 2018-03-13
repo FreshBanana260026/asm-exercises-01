6dd25dad9effab9426f2e5b2b5fe87939f8475b4
Patrik <260026@via.dk>
2018-03-13 11:37:22 +0100

    exercise5 final version

--- exercises1_5/exercises1_5/main.asm
+++ exercises1_5/exercises1_5/main.asm
@@ -8,6 +8,11 @@
 
 ldi r17, 3			; load 3 into register r17
 ldi r18, 33			; load 33 into register r18
-mul r17, r18		; multiply r17 and r18 into r0
-mov r16, r0			; copy r0 to r16
+ldi r19, 55			; load 55 into r19
+ldi r20, 12			; load 12 into r20
+ldi r16, 34			; load 34 into r21
+mul r17, r18		; multiply r17 and r18, store result into r0
+add r16, r19		; r16 += 55
+sub r16, r20		; r16 -= 12
+add r16, r0			; r16 += r0(99)
 nop					; placeholder for breakpoint
