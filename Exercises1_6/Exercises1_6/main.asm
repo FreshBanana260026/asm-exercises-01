425d301cac9567c910ceb13734380c45d3c4754b
Patrik <260026@via.dk>
2018-03-15 12:33:04 +0100

    exercise7

--- Exercises1_6/Exercises1_6/main.asm
+++ Exercises1_6/Exercises1_6/main.asm
@@ -8,6 +8,48 @@
 							; SETUP
     ldi r16, 0xff           ; load the bit pattern 1111 1111 into r16
     out ddra, r16           ; write the bit pattern to port a's data direction register
+	ldi r17, 0b1111_1011	;setting led on
+	ldi r18, 0b1111_1111	;setting led off
+	ldi r19, 100
+	ldi r20, 100
+	ldi r21, 100
+	ldi r22, 100
+	ldi r23, 100
+	ldi r24, 100
+	
+loop:
+	out porta, r17			; putting r17 bit into portA
+delay1:
+	delay2:
+		DEC r20				; r19--
+		BREQ end2
+		rjmp delay2
+	end2:
+	delay5:
+		DEC r23				; r19--
+		BREQ end5
+		rjmp delay5
+	end5:
+	DEC r19					; r19--
+	BREQ end1
+	rjmp delay1
+end1:
+	nop
+	out porta, r18			; putting r18 bit into portA
 
-	ldi r17, 0b1111_1011	;setting which leds should be on
-	out porta, r17			;putting r17 bit into portA
+delay3:
+	delay4:
+		DEC r21					; r19--
+		BREQ end4
+		rjmp delay4
+	end4:
+	delay6:
+		DEC r24				; r19--
+		BREQ end6
+		rjmp delay6
+	end6:
+	DEC r22					; r19--
+	BREQ end3
+	rjmp delay3
+end3:
+	rjmp loop
